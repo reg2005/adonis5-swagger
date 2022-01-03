6e40bf7f690e5cdeeffeb541ec96b14d8871947c
vladyslavparashchenko <vladyslavparashchenko37@gmail.com>
2022-01-03 20:22:10 +0200

    chore: update packages

--- japaFile.js
+++
@@ -1,9 +0,0 @@
-const { configure } = require('japa')
-const { argv } = require('yargs')
-
-const { files = ['test/**/*.spec.ts'], grep } = argv
-
-configure({
-	files: Array.isArray(files) ? files : [files],
-	grep: grep,
-})
